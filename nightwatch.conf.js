ea43138d51549456128a544b83d917b1b3308f47
Satabdi Sikdar <satabdi.sikdar@velotio.com>
2021-02-15 03:47:13 +0530

    tweaks

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,19 +1,21 @@
 const chromedriver = require('chromedriver');
+
+
 module.exports = {
-    src_folders: ["tests"],  
-    webdriver: {
-      start_process: true,
-      port: 4444,
-      server_path: chromedriver.path,
-      cli_args: ['--port=4444']
-    },
-    globals:{
-        waitForConditionTimeout: 10000,
-      },
+    src_folders: ["tests"],    
   
     test_settings: {
       default: {
-        //launch_url: 'https://nightwatchjs.org',
+        //launch_url: launchUrl || 'https://nightwatchjs.org',
+        webdriver: {
+          start_process: true,
+          port: 4444,
+          server_path: chromedriver.path,
+          cli_args: ['--port=4444']
+        },
+        globals:{
+            waitForConditionTimeout: 10000,
+          },
         desiredCapabilities: {
             browserName: 'chrome',
             javascriptEnabled: true,
--- tests/sample.js
+++ tests/sample.js
@@ -1,29 +1,33 @@
 var supertest = require('supertest');
 const website = 'https://reqres.in/';
-const request = supertest(website); 
+const request = supertest(website);
 let webUrl;
 
 try{
   test("GET user output", async done =>{
     await request.get(`api/users`).expect(200).then((response) =>{
           console.log("GET RESPONSE : ", response.body);
-          webUrl= response.body.data[0].avatar;
-      })
+          for(let i=0; i<response.body.data.length; i++){
+            if(response.body.data[i].id%3 == 0){
+              webUrl= response.body.data[i].avatar;
+              console.log(webUrl);
+              describe('Hit urls',function(){
+                test('demo', function(browser){
+                browser.url(webUrl).useXpath().waitForElementVisible('//body').assert.elementPresent('//body');
+                browser.end();
+              } )})                                             
+            }
+            else{
+              console.log("NO URL!  ", response.body.data[i].id);
+            }          
+      }
+  })
   })
 }
 catch(err){
   console.log("Exception: ", err)
 }
 
-module.exports = {
-    'Demo test' : function(browser) {
-      browser
-        .url(webUrl)
-        .waitForElementVisible('body').pause(3000)
-        .end();
-    
-    }
-  };
 
   
     
